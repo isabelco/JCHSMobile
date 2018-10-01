d6403aa4f2a1c9743821567537cfe0f55fa8824b
Garett Pascual-Folster <garettpascual-folster@Melanies-iMac.local>
2018-09-30 19:12:47 -1000

    Initial commit

---
+++ app/src/main/java/com/example/garettpascual_folster/jchsmobile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.garettpascual_folster.jchsmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
